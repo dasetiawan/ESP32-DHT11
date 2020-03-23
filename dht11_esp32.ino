cf4757659425c7e683f3b0b2680d8e1c50e96add
Dwi Setiawan <38515985+dasetiawan@users.noreply.github.com>
2020-03-23 22:02:34 +0700

    Update dht11_esp32.ino

--- dht11_esp32.ino
+++ dht11_esp32.ino
@@ -1,34 +1,76 @@
+#inclide <ArduinoJson.h>      // add ArduinoJson library v5.9.0
 #include <Adafruit_Sensor.h>  // add Adafruit_Sensor library
+#include <PubSubClient.h>     // add PubSubClient library
 #include <DHT.h>        // add DHT library
 #define DHTPIN 4        // define pin which is connected to DHT sensor
 #define DHTTYPE DHT11   // define type of sensor, if you're another sensor change it
 
+const char* ssid = "ssid";             // your wifi ssid
+const char* password = "password";     // your wifi password
+const char* mqttServer = "192.168.1.100";   // mqtt server address
+const int mqttPort = 1883;                  // mqtt port
+const char* mqttUser = "mqtt_username";     // your mqtt user
+const char* mqttPassword = "mqtt_password"; // your mqtt password
+
+float t;  // variable for temperature
+float h;  // variable for humidity
+
 DHT dht(DHTPIN, DHTTYPE);
+WiFiClient espClient;
+PubSubClient client(espClient);
  
 void setup()
 {
 Serial.begin(115200);
 dht.begin();
+
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.println("Connecting to WiFi..");
+  }
+  Serial.println("Connected to the WiFi network");
+  
+  client.setServer(mqttServer, mqttPort);
+  while (!client.connected()) {
+    Serial.println("Connecting to MQTT...");
+    if (client.connect("ESP32Client", mqttUser, mqttPassword )) {
+      Serial.println("connected");
+    } else {
+      Serial.print("failed with state ");
+      Serial.print(client.state());
+      delay(2000);
+    }
+  }
 }
  
 void loop() {
-// read humidity as percentage and temperature as celcius
-float t = dht.readTemperature();
-float h = dht.readHumidity();
- 
-// check if any reads failed
-if (isnan(t) || isnan(h)) {
-Serial.println("Failed to read from DHT sensor!");
-return;
-}
- 
-// print the result to serial monitor or terminal
-Serial.print("Temperature: "); 
-Serial.print(t);
-Serial.print(" *C\t");
-Serial.print("Humidity: ");
-Serial.print(h);
-Serial.println(" %");
+readPZEM();
+    StaticJsonBuffer <300> JSONbuffer;
+    JsonObject& JSONencoder = JSONbuffer.createObject();
+    
+    JSONencoder["device"] = "ESP32";
+    JSONencoder["sensor type"] = "DHT11";
+    JSONencoder["temperature"] = t;
+    JSONencoder["humidity"] = h;
 
-delay(2000);
+    char JSONmessageBuffer[100];
+    JSONencoder.printTo(JSONmessageBuffer, sizeof(JSONmessageBuffer));
+    Serial.println("Sending message to MQTT topic..");
+    Serial.println(JSONmessageBuffer);
+     
+    if (client.publish("esp/dht11", JSONmessageBuffer) == true) {
+      Serial.println("Success sending message");
+    } else {
+      Serial.println("Error sending message");
+    }
+    
+    delay(2000);
+    client.loop();
+}
+
+// function to read temperature and humidity 
+void readData(){
+ t = dht.readTemperature();
+ h = dht.readHumidity();
 }
